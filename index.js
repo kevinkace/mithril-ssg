181a9dacc408fb3e966ca4155696fb88995af70f
Kevin Cameron <kevinacameron@hotmail.com>
2019-03-03 18:06:03 -0800

    Move clearing /dist to main fn

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const util               = require("util");
+const { mkdir : _mkdir } = require("fs");
+
+const mkdir = util.promisify(_mkdir);
+
+const rmfr   = require("rmfr");
 const globby = require("globby");
 
+const log        = require("./lib/log");
 const readFiles  = require("./lib/readFiles");
 const render     = require("./lib/render");
 const writeFiles = require("./lib/writeFiles");
 const handleErrs = require("./lib/handleErrs");
 
-module.exports = function mithrilSsg(glob = "./src/pages/**/*.js") {
+module.exports = async function mithrilSsg(glob = "./src/pages/**/*.js") {
+    log("deleting", "/dist");
+
+    await rmfr("./dist");
+    await mkdir("./dist");
+
     return globby(glob)
         .then(readFiles)
         .then(render)
